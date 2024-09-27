175426f3a451abfe161637afc43d44111a0f88de
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-27 11:44:47 +0200

    Création : mise en place du répertoire des pages du site

---
+++ src/pages/index.php
@@ -0,0 +1,11 @@
+<header>
+    <h1>PokéSearch</h1>
+</header>
+<main>
+    <form action="element.php" method="get">
+        <div class="search-container">
+            <label for="search">Rechercher un Pokémon :</label>
+            <input type="text" id="search" name="search" placeholder="Tapez un nom de Pokémon...">
+        </div>
+    </form>
+</main>
